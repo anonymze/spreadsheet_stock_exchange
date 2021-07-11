a515cbc57073a7a1b008a19629a87d9c8522a042
yann <ymetier@openstudio;fr>
2021-07-11 23:40:07 +0200

    ok

--- src/Services/InsertInSpreadsheet.php
+++ src/Services/InsertInSpreadsheet.php
@@ -42,21 +42,59 @@ class InsertInSpreadsheet {
         "26" => "Z"
     ];
 
-    protected $firstFormulaCell1 = "";
-    protected $firstFormulaCell2 = "";
-    protected $firstFormulaCell3 = "";
-    protected $firstFormulaCell4 = "";
-    protected $firstFormulaCell5 = "";
-    protected $secondFormulaCell1 = "";
-    protected $secondFormulaCell2 = "";
-    protected $secondFormulaCell3 = "";
-    protected $secondFormulaCell4 = "";
-    protected $secondFormulaCell5 = "";
+    protected $totalBusiness = "";
+    protected $totalBusiness2 = "";
+    protected $totalBusiness3 = "";
+    protected $totalBusiness4 = "";
+    protected $totalBusiness5 = "";
+    protected $totalBusiness6 = "";
+    protected $totalBusiness7 = "";
+    protected $brutBusiness = "";
+    protected $brutBusiness2 = "";
+    protected $brutBusiness3 = "";
+    protected $brutBusiness4 = "";
+    protected $brutBusiness5 = "";
+    protected $brutBusiness6 = "";
+    protected $brutBusiness7 = "";
+    protected $generalSellAdministrative = "";
+    protected $generalSellAdministrative2 = "";
+    protected $generalSellAdministrative3 = "";
+    protected $generalSellAdministrative4 = "";
+    protected $generalSellAdministrative5 = "";
+    protected $generalSellAdministrative6 = "";
+    protected $generalSellAdministrative7 = "";
+    protected $openExploitation = "";
+    protected $openExploitation1 = "";
+    protected $openExploitation2 = "";
+    protected $openExploitation3 = "";
+    protected $openExploitation4 = "";
+    protected $openExploitation5 = "";
+    protected $openExploitation6 = "";
+    protected $interestCharge = "";
+    protected $interestCharge2 = "";
+    protected $interestCharge3 = "";
+    protected $interestCharge4 = "";
+    protected $interestCharge5 = "";
+    protected $interestCharge6 = "";
+    protected $interestCharge7 = "";
+    protected $netBenefice = "";
+    protected $netBenefice2 = "";
+    protected $netBenefice3 = "";
+    protected $netBenefice4 = "";
+    protected $netBenefice5 = "";
+    protected $netBenefice6 = "";
+    protected $netBenefice7 = "";
+    protected $researchDevelopment = "";
+    protected $researchDevelopment2 = "";
+    protected $researchDevelopment3 = "";
+    protected $researchDevelopment4 = "";
+    protected $researchDevelopment5 = "";
+    protected $researchDevelopment6 = "";
+    protected $researchDevelopment7 = "";
 
-    protected $firstArrayFormulaCell = [];
-    protected $secondArrayFormulaCell = [];
-
-    protected $firstCountDown = 0;
+    protected $firstCurrent = "";
+    protected $secondCurrent = "";
+    protected $firstCountDown = -1;
     protected $secondCountDown = -1;
 
     public function __construct(ParameterBagInterface $params, $keyExtend)
@@ -103,26 +141,83 @@ class InsertInSpreadsheet {
 
                 $val =  preg_replace('/-?\s?/u', '', $tdTableReconstruct[$i]);
 
-                if ($this->firstCountDown > 0) {
-                    $this->firstArrayFormulaCell[] = $val ?: "1";
-                    $this->firstCountDown--;
+                /**
+                 * global formula
+                 */
+                if ($this->firstCountDown >= 0 && $this->firstCountDown <= $count) {
+                    ++$this->firstCountDown;
+                    $val = $val ?: "1";
+
+                    if ($this->firstCountDown === 1) {
+                        $this->{$this->firstCurrent} = $val;
+                    } else {
+                        $this->{$this->firstCurrent.$this->firstCountDown} = $val;
+                    }
                 }
 
-                if ($this->secondCountDown > 0) {
-                    $this->secondArrayFormulaCell[] = $val ?: "1";
-                    $this->secondCountDown--;
+
+                if ($this->secondCountDown >= 0 && $this->secondCountDown < $count) {
+                    ++$this->secondCountDown;
+                    $val = $val ?: "1";
+
+                    if ($this->secondCountDown === 1) {
+                        $this->{$this->secondCurrent} = $val;
+                    } else {
+                        $this->{$this->secondCurrent.$this->secondCountDown} = $val;
+                    }
                 }
 
-                if ($this->secondCountDown === 0) {
-                    $this->sheet->setCellValue(self::$alaphabetNumeric[$count + 2] . ($this->countRow), "MARGE BRUTE");
+                if ($this->secondCountDown === $count - 1) {
+                    for($c = 1; $c <= $count; $c++) {
+                        $color = false;
 
-                    for($c = 0, $cMax = count($this->firstArrayFormulaCell); $c < $cMax; $c++) {
-                        $val1 = (int)$this->firstArrayFormulaCell[$c];
-                        $val2 = (int)$this->secondArrayFormulaCell[$c];
+                        // calcul formula basic
+                        if ($c === 1) {
+                            switch($this->secondCurrent) {
+                                case "brutBusiness":
+                                    $this->sheet->setCellValue(self::$alaphabetNumeric[$count + 3] . ($this->countRow), "MARGE BRUTE");
+                                    break;
+                                case "generalSellAdministrative":
+                                    $this->sheet->setCellValue(self::$alaphabetNumeric[$count + 3] . ($this->countRow), "FRAIS D'EXPLOIT");
+                                    break;
+                                case "interestCharge":
+                                    $this->sheet->setCellValue(self::$alaphabetNumeric[$count + 3] . ($this->countRow), "CHARGE D'INTERET");
+                                    break;
+                                case "netBenefice":
+                                    $this->sheet->setCellValue(self::$alaphabetNumeric[$count + 3] . ($this->countRow), "MARGE NET");
+                                    break;
+                                case "researchDevelopment":
+                                    $this->sheet->setCellValue(self::$alaphabetNumeric[$count + 3] . ($this->countRow), "RESERCHE DEVELOPPEMENT");
+                                    break;
+                            }
 
-                        if($val1 > 0 && $val2 > 0) {
-                            $result = ($val2 / $val1) * 100;
-                            if ($result > 65 && $result < 100) {
+                            if ($this->{$this->secondCurrent} > 0 && $this->{$this->firstCurrent} > 0) {
+                                $result = ($this->{$this->secondCurrent} / $this->{$this->firstCurrent}) * 100;
+                            }
+                        } else if ($this->{$this->secondCurrent . $c} > 0 && $this->{$this->firstCurrent . $c} > 0) {
+                            $result = ($this->{$this->secondCurrent . $c} / $this->{$this->firstCurrent . $c}) * 100;
+                        }
+
+                        if (isset($result)) {
+                            switch($this->secondCurrent) {
+                                case "brutBusiness":
+                                    $color = $result > 65;
+                                break;
+                                case "generalSellAdministrative":
+                                    $color = $result < 30;
+                                    break;
+                                case "interestCharge":
+                                    $color = $result < 15;
+                                    break;
+                                case "netBenefice":
+                                    $color = $result > 35;
+                                    break;
+                                case "researchDevelopment":
+                                $color = $result < 5;
+                                break;
+                            }
+
+                            if ($color === true) {
                                 $this->sheet->setCellValue(self::$alaphabetNumeric[$count + (3 + $c)] . $this->countRow, $result)
                                     ->getStyle(self::$alaphabetNumeric[$count + (3 + $c)] . $this->countRow)
                                     ->getFill()
@@ -133,24 +228,63 @@ class InsertInSpreadsheet {
                                 $this->sheet->setCellValue(self::$alaphabetNumeric[$countAlphabet + (3 + $c)] . $this->countRow, $result);
                             }
                         }
-
-                        if ($c === ($count - 2)) {
-                            $this->secondCountDown = -1;
-                        }
                     }
 
-
+                    $this->secondCountDown = -1;
+                    $this->firstCountDown = -1;
                 }
 
+                /**
+                 * FIRST FORMULA MARGE BRUT
+                 */
                 if ($val === "Chiffred’affairestotal") {
-                    $this->setupFirstRows($countAlphabet, $this->countRow);
-                    $this->firstCountDown = $count -1;
-                    var_dump($this->firstCountDown);
+                    $this->firstCountDown = 0;
+                    $this->firstCurrent = "totalBusiness";
                 }
 
                 if ($val === "Bénéficebrut") {
-                    $this->setupSecondRows($countAlphabet, $this->countRow);
-                    $this->secondCountDown = $count -1;
+                    $this->secondCountDown = 0;
+                    $this->secondCurrent = "brutBusiness";
+                }
+
+                /**
+                 * SECOND FORMULA FRAIS D'EXPLOITATION
+                 */
+                if ($val === "Ventesgénéralesetadministratives") {
+                    $this->secondCountDown = 0;
+                    $this->firstCurrent = "brutBusiness";
+                    $this->secondCurrent = "generalSellAdministrative";
+                }
+
+                /**
+                 * THIRD FORMULA FRAIS D'EXPLOITATION
+                 */
+                if ($val === "Bénéficeouperted’exploitation") {
+                    $this->firstCountDown = 0;
+                    $this->firstCurrent = "openExploitation";
+                }
+
+                if ($val === "Charged’intérêt") {
+                    $this->secondCountDown = 0;
+                    $this->secondCurrent = "interestCharge";
+                }
+
+                /**
+                 * FOURTH FORMULA MARGE NET
+                 */
+                if ($val === "Bénéficenet") {
+                    $this->secondCountDown = 0;
+                    $this->firstCurrent = "totalBusiness";
+                    $this->secondCurrent = "netBenefice";
+                }
+
+                /**
+                 * FIFTH FORMULA R AND D
+                 */
+                if ($val === "Développementdelarecherche") {
+                    $this->secondCountDown = 0;
+                    $this->firstCurrent = "brutBusiness";
+                    $this->secondCurrent = "researchDevelopment";
                 }
 
                 if(is_numeric($val)) {
@@ -165,25 +299,5 @@ class InsertInSpreadsheet {
         }
 
         $this->writer->save(self::$path);
-    }
-
-    public function setupFirstRows($countAlphabet, $countRow) {
-        $this->firstFormulaCell1 = self::$alaphabetNumeric[$countAlphabet + 1] . $countRow;
-        $this->firstFormulaCell2 = self::$alaphabetNumeric[$countAlphabet + 2] . $countRow;
-        $this->firstFormulaCell3 = self::$alaphabetNumeric[$countAlphabet + 3] . $countRow;
-        $this->firstFormulaCell4 = self::$alaphabetNumeric[$countAlphabet + 4] . $countRow;
-        $this->firstFormulaCell5 = self::$alaphabetNumeric[$countAlphabet + 5] . $countRow;
-    }
-
-    public function setupSecondRows($countAlphabet, $countRow) {
-        $this->secondFormulaCell1 = self::$alaphabetNumeric[$countAlphabet + 1] . ($countRow);
-        $this->secondFormulaCell2 = self::$alaphabetNumeric[$countAlphabet + 2] . ($countRow);
-        $this->secondFormulaCell3 = self::$alaphabetNumeric[$countAlphabet + 3] . ($countRow);
-        $this->secondFormulaCell4 = self::$alaphabetNumeric[$countAlphabet + 4] . ($countRow);
-        $this->secondFormulaCell5 = self::$alaphabetNumeric[$countAlphabet + 5] . ($countRow);
-    }
-
-    public function setupThirdRows() {
-
     }
 }
